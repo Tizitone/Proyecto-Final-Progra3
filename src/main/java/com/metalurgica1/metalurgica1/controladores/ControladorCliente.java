1b32875a0db0dbed63981e7b909c24065df41b74
tizitone <tizitone@gmail.com>
2026-06-02 10:43:51 -0300

    Cors Config necesario

--- src/main/java/com/metalurgica1/metalurgica1/controladores/ControladorCliente.java
+++ src/main/java/com/metalurgica1/metalurgica1/controladores/ControladorCliente.java
@@ -4,15 +4,52 @@ import com.metalurgica1.metalurgica1.modelo.Cliente;
 import com.metalurgica1.metalurgica1.modelo.Empleado;
 import com.metalurgica1.metalurgica1.repositorio.IClienteRepository;
 import com.metalurgica1.metalurgica1.repositorio.IEmpleadoRepository;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin(origins = "http://localhost:8080")
 public class ControladorCliente {
+
+    @Component
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public class MyCorsConfig implements Filter {
+
+        @Override
+        public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+            final HttpServletResponse response = (HttpServletResponse) res;
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, enctype");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+                response.setStatus(HttpServletResponse.SC_OK);
+            } else {
+                chain.doFilter(req, res);
+            }
+        }
+
+        @Override
+        public void destroy() {
+        }
+
+        @Override
+        public void init(FilterConfig config) throws ServletException {
+        }
+    }
+
     private final IClienteRepository iClienteRepository;
 
     public ControladorCliente(IClienteRepository iClienteRepository) {
