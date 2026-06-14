a6acda4fffde2ca2e21dcea260f00523e80007da
steyker4 <carlostananta04@gmail.com>
2026-06-13 21:51:15 -0300

    cambios pequeños que faltaron hacer

--- src/main/java/com/metalurgica1/metalurgica1/dto/SolicitudDTO.java
+++ src/main/java/com/metalurgica1/metalurgica1/dto/SolicitudDTO.java
@@ -1,15 +1,15 @@
 package com.metalurgica1.metalurgica1.DTO;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 
 public record SolicitudDTO(
         Long id,
         @NotNull(message = "El nombre no puede ser nulo")
         @NotBlank(message = "El nombre no puede estar en blanco")
         String nombre,
+        
+        @NotBlank(message = "No puede tener el email en blanco")
+        @Email(message = "Debe ser un email valido")
         String email,
         @NotNull(message = "El telefono no puede ser nulo")
         @Pattern(regexp = "^\\d{7,11}$", message = "El telefono debe contener entre 7 y 11 digitos numericos")
