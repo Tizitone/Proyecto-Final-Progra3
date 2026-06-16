6dda190c8328852c66ebb47cd727c301432d6b8a
Tiziano <tizitone@gmail.com>
2026-06-15 23:55:26 -0300

    html security connection

--- src/main/resources/static/IngresoUsuario/script.js
+++ src/main/resources/static/IngresoUsuario/script.js
@@ -1,16 +1,18 @@
-document.getElementById('loginForm').addEventListener('submit', function(evento) {
+const formLogin = document.getElementById('loginForm')
+
+formLogin.addEventListener('submit', async function(evento) {
     evento.preventDefault();
 
-    const usuario = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
     const contrasenia = document.getElementById('password').value;
 
     let isValid = true;
 
-    if (!usuario.match(/[a-zA-Z0-9]{5,}/)) {
-        document.getElementById('usernameError').textContent = "El nombre de usuario debe contener al menos 5 carácteres alfanumericos."
+    if (!email.match(/[a-zA-Z0-9]{5,}/)) {
+        document.getElementById('emailError').textContent = "El email debe contener al menos 5 carácteres alfanumericos."
         isValid = false;
     } else {
-        document.getElementById('usernameError').textContent = '';
+        document.getElementById('emailError').textContent = '';
     }
 
     if (!contrasenia.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/)) {
@@ -21,17 +23,42 @@ document.getElementById('loginForm').addEventListener('submit', function(evento)
     }
 
     if (isValid) {
-        simularRequestAServer(usuario, contrasenia);
+
+        try{
+            const response = await fetch("http://localhost:8080/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, contrasenia})
+            });
+
+            if (!response.ok) {
+                throw new Error('http error! status: ${response.status}');
+            }
+
+            const data = await response.json();
+            console.log("login logrado: ", data);
+            alert("login logrado! Redirigiendo...");
+            
+            setTimeout(function() {
+                window.location.href = "IdeaProjects\Proyecto-Final-Progra3\src\main\resources\static\CRUD metalurgica\HTML metalurgica.html";
+            }, 3000);
+
+        } catch {
+
+        }
+        simularRequestAServer(email, contrasenia);
     }
 });
 
-function simularRequestAServer(usuario, contrasenia) {
+function simularRequestAServer(email, contrasenia) {
     const statusElement = document.getElementById('loginStatus');
 
     setTimeout(() => {
         statusElement.textContent = 'Login Logrado! redirigiendo...'
-        statusElement.style.color = 'green';
-    }, 1000);
+        statusElement.style.color = 'red';
+    }, 5000);
 }
 
 
