d4b6f0560f987583e600901c9f8ec4aee0edd66b
Franco Babbicola <franco.babbicola@gmail.com>
2026-06-23 22:45:48 -0300

    -implementacion api Email

--- src/main/java/com/metalurgica1/metalurgica1/Config/OpenApiConfig.java
+++ src/main/java/com/metalurgica1/metalurgica1/Config/OpenApiConfig.java
@@ -1,6 +1,5 @@
 package com.metalurgica1.metalurgica1.Config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
